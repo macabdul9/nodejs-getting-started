18b38a322300fbcef75e5e7da5d11857a1e920a7
macabdul9 <abdulwaheed1513@gmail.com>
2020-03-13 01:53:42 +0530

    debugging node

--- app.js
+++ app.js
@@ -1,45 +1,7 @@
 const http = require("http");
-const fs = require("fs");
+const routes = require('./routes.js')
 
-// function reqListener(req, res) {
+const server = http.createServer(routes);
 
-// }
-
-let count = 1;
-// event driven approach
-const server = http.createServer((req, res) => {
-  const url = req.url
-  const method = req.method
-  if(url==='/'){
-    res.write("<html>");
-    res.write("<head><title>This is nodejs server</title></head>");
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
-    res.write("</html>");
-    return res.end();
-  }
-  if(url==='/message' && method==='POST'){
-        const body = [];
-        req.on('data', (chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        })
-        return req.on('end', ()=> {
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            });
-            
-        })
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>This is nodejs server</title></head>");
-  res.write("<body><h1>Welcome to nodejs server</h1></body>");
-  res.write("</html>");
-  res.end();
-});
-
-server.listen(3000);
+console.log('hello')
+server.listen(3001);
