e5a57491878448598cec75237dcf3e4f0f660157
macabdul9 <abdulwaheed1513@gmail.com>
2020-03-15 03:26:50 +0530

    dynamic view HTMLish template added

--- routes/admin.js
+++ routes/admin.js
@@ -6,20 +6,24 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
-
 const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {pageTitle:'Add Product'});
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  // console.log(req.body);
-  products.push({title : req.body.title})
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-exports.router = router;
+exports.routes = router;
 exports.products = products;
