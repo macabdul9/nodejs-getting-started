d41e8cb9709da8d3a450023bc97c258e841d0c57
macabdul9 <abdulwaheed1513@gmail.com>
2020-05-06 23:10:11 +0530

    mongodb completed

--- util/database.js
+++ util/database.js
@@ -1,19 +1,30 @@
 const mongodb = require("mongodb")
 
+let _db;
 const mongoConnect = callback => {
 
   const MongoClient = mongodb.MongoClient;
-  uri = 'mongodb+srv://abdul:t78w2jvXZhnKHRm9@nodejs-db-xx3ji.mongodb.net/test?retryWrites=true&w=majority'
+  uri = 'mongodb+srv://abdul:abdul@nodejs-db-xx3ji.mongodb.net/shop?retryWrites=true&w=majority'
   new MongoClient(uri, { useUnifiedTopology: true }).connect()
   .then(client => {
     console.log("CONNECTED");
+    _db = client.db('shop');
     callback(client)
   })
   .catch(err => {
     console.log(err);
+    throw err;
   })
-
 } 
 
-module.exports = mongoConnect;
+const getDb  = () => {
+  if(_db){
+    return _db;
+  }
+  throw "No database found";
+}
+
+// module.exports = mongoConnect;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
 
